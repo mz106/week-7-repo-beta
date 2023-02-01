8ff036cc56f713aa0c320dd6b0f95ecc8a7fa36c
Michael Z <michaelzwierzanski@gmail.com>
2023-02-01 10:17:15 +0000

    add notes step 1b

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // Step 1 - Add html route
 // Step 2 - Add get route /books
+// Step 3 - Add post route /books
 
 const express = require("express");
 
@@ -30,6 +31,14 @@ const html = `
 // raise differences in using the static route the previous day
 // State that previsouly, we just connected to the static files with Express,
 // but now we are sending a response using the below code to send a string.
+// The response is to a http GET request that will automatically be sent by the
+// browser (the user agent)
+
+// Go to the browser and navigate to http://localhost:5001/html (or the particular that you are using).
+// Go to the network tab on the inspection tools and show the get request to the server address (http://localhost:5001/html).
+// Explain that this is the GET request to 'get' the data from the server from the particular route,
+// in this case '/html'
+// Refer back to slides and explain the journey in the context of this route.
 
 app.use("/html", (req, res) => {
   res.send(html);
@@ -53,6 +62,29 @@ app.get("/book", (req, res) => {
   };
 
   res.status(200).json(successResponse);
+});
+
+// step 3a - write post route /book
+
+app.post("/book", (req, res) => {
+  console.log("/src/server.js POST /book req.body: ", req.body);
+
+  // this is just simple manipulation of the body.
+
+  // refer back to slides.
+
+  const bookStringObj = {
+    titleString: `I really like ${req.body.title}`,
+    authorString: `${req.body.author} is a good author`,
+    genreString: `${req.body.genre} is a great genre`,
+  };
+
+  const successResponse = {
+    message: "success",
+    bookStringObj: bookStringObj,
+  };
+
+  res.status(201).json(successResponse);
 });
 
 app.listen(5001, () => console.log("Server is listening on port 5001"));
